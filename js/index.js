d6d535615e1d9d29e32114de6bf8ee0799552302
Olamgir <olamgir202@gmail.com>
2023-09-02 06:12:09 +0500

    added swiper

--- js/index.js
+++ js/index.js
@@ -1,29 +1,37 @@
 let btn = document.querySelector('.burger')
 let list = document.querySelector('.list')
+let span = document.querySelector('.burger')
 let f1 = ()=>{
   list.classList.toggle('active')
-  console.log(btn);
+  span.classList.toggle('burgerx')
+  console.log(span);
 }
 
 btn.onclick = ()=>{f1()}
 
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
+  },  
+  keyboard: {
+    enabled: true,
+    onlyViewport: true,
+    pageUpDown: true
+  },
+  mousewheel: {
+    sensitivity: 1,
+    evenstarget: '.slide-image'
+  },
+  autoheight: true,
+  watchoverflow: true
+});
 
 
 // fetch('https://dummyjson.com/carts', {
